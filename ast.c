6a6702d6fa747e69ff32265ac263529a315251a4
Nur Kholid <cocokcocok33@gmail.com>
2024-04-18 18:54:33 +0700

    WIP Abstract Syntax Tree

--- ast.c
+++ ast.c
@@ -1,1 +1,9 @@
 #include "ast.h"
+#include <stdlib.h>
+
+ast_node *number_expr_ast_create(double value) {
+  ast_node *node = malloc(sizeof(ast_node));
+  node->type = NUMBER;
+  node->number.value = value;
+  return node;
+}
--- ast.h
+++ ast.h
@@ -2,6 +2,14 @@
 #define AST_H
 
 struct ast_node;
+typedef enum ast_node_type_e {
+  NUMBER,
+  VARIABLE,
+  BINARY_EXPR,
+  CALL,
+  PROTOTYPE,
+  FUNCTION,
+} ast_node_type_e;
 
 typedef enum binary_operations_expression {
   ADD,
@@ -39,6 +47,18 @@ typedef struct {
   struct ast_node *prototype, *body;
 } function_ast;
 
+typedef struct ast_node {
+  ast_node_type_e type;
+  union {
+    number_expr_ast number;
+    variable_expr_ast variable;
+    binary_expr_ast binary_expr;
+    call_expr_ast call;
+    prototype_ast prototype;
+    function_ast function;
+  };
+} ast_node;
+
 ast_node *number_expr_ast_create(double value);
 ast_node *variable_expr_ast_create(char *name);
 ast_node *binary_expr_ast_create(binary_operations_expression operations,
@@ -46,5 +66,6 @@ ast_node *binary_expr_ast_create(binary_operations_expression operations,
 ast_node *call_expr_ast_create(char *call, char **args, int arg_count);
 ast_node *prototype_ast_create(char *name, char **args, int arg_count);
 ast_node *function_ast_create(ast_node *prototype, ast_node *body);
+void node_ast_free(struct ast_node *node);
 
 #endif // !AST_H
