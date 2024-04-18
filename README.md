563bf8ded993fcabb1ea12784b8d107d807823b2
Nur Kholid <cocokcocok33@gmail.com>
2024-04-18 16:27:57 +0700

    add definition Abstract Syntax Tree

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# 💩-Lang
+# Kaleidoscope-Lang
 
 I created my own language just for fun, using LLVM and purely learning from the kaleidoscope documentation on the LLVM site.
 
 |Ref | Link |
 | --- | ----------- |
 | Documentation LLVM | [LLVM](https://llvm.org/docs/tutorial/MyFirstLanguageFrontend/LangImpl01.html) | 
+| Abstract Syntax Tree in C | [AST](https://keleshev.com/abstract-syntax-tree-an-example-in-c/) |
--- ast.h
+++ ast.h
@@ -1,4 +1,42 @@
 #ifndef AST_H
 #define AST_H
 
+struct Ast_Node;
+
+typedef enum BinaryOperationsExpression {
+  ADD,
+  SUB,
+  MUL,
+  DIV,
+} BinaryOperationsExpression;
+
+typedef struct {
+  double value;
+} NumberExprAST;
+
+typedef struct {
+  char *name;
+} VariableExprAST;
+
+typedef struct {
+  BinaryOperationsExpression operations;
+  struct Ast_Node *lhs, *rhs;
+} BinaryExprAST;
+
+typedef struct {
+  char *calle;
+  struct Ast_Node **args;
+  unsigned int arg_count;
+} CallExprAST;
+
+typedef struct {
+  char *name;
+  char **args;
+  unsigned int arg_count;
+} PrototypeAST;
+
+typedef struct {
+  struct Ast_Node *prototype, *body;
+} FunctionAST;
+
 #endif // !AST_H
